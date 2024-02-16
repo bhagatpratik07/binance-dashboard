d70c7d33297f7cc291c13284f6ff950800bb97a2
Pratik <bhagatpratik.2002@gmail.com>
2024-02-16 17:08:04 +0530

    price color update on frontend

--- client/src/components/Coin.jsx
+++ client/src/components/Coin.jsx
@@ -5,23 +5,50 @@ const socket = io("http://localhost:3000");
 
 export default function Coin() {
   const [coins, setCoins] = useState([]);
+  const [prevCoins, setPrevCoins] = useState({});
 
   useEffect(() => {
-    socket.on("connect", console.log("connected"));
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+
     socket.on("update", (data) => {
       setCoins(data);
-      console.log(data);
+      updatePrevCoins(data);
     });
   }, []);
+
+  const updatePrevCoins = (data) => {
+    setPrevCoins((prev) => {
+      const updatedPrevCoins = {};
+      data.forEach((coin) => {
+        if (prev[coin.symbol]) {
+          // Update previous value
+          updatedPrevCoins[coin.symbol] = prev[coin.symbol];
+        } else {
+          // Set previous value as current value
+          updatedPrevCoins[coin.symbol] = coin.price;
+        }
+      });
+      return updatedPrevCoins;
+    });
+  };
+
+  const getColor = (symbol, price) => {
+    const prevPrice = prevCoins[symbol];
+    return price > prevPrice ? "green" : price < prevPrice ? "red" : "black";
+  };
 
   return (
     <div>
       <div>
         {coins.map((coin) => (
-          <>
+          <div key={coin.symbol}>
             <h3>{coin.symbol}</h3>
-            <h4>{coin.price}</h4>
-          </>
+            <h4 style={{ color: getColor(coin.symbol, coin.price) }}>
+              {coin.price}
+            </h4>
+          </div>
         ))}
       </div>
     </div>
